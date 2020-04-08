0c2ede14f48b983f7f8aff07fdb62320bc347b59
Macy <59153195+Macymo@users.noreply.github.com>
2020-04-08 16:26:45 -0400

    populating time slots differently than previous code

--- script.js
+++ script.js
@@ -1,40 +1,90 @@
 var day = document.getElementById("currentDay");
 //console.log("current day");
 var schedule = document.getElementById("scheduler");
+var mainRow = document.getElementById("rows");
+var hourClmn = document.getElementById("hour");
+var scheduleInput = document.getElementById("events");
+var saveBtn = document.getElementById("save");
+var btn = document.getElementById("saveButton");
 
 
 $(day).text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
-var timeOfDay = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM"];
-for (var i = 0; i < timeOfDay.length; i++) {
-var row = $("<div>"); 
-row.attr("class","row");
-console.log(timeOfDay);
+var hour = [];
+var hours = moment().format('LT'); 
+//console.log(hours);
+for(var i = 0; i < 11; i++){
+    mainRow = $("<div>");
+    mainRow.attr("class", "row");
+    console.log(mainRow);
 
-//inside of each row
-var hourCol = $("<div>");
-hourCol.text(timeOfDay[i]);
-hourCol.attr("class", "col hour");
-row.append(hourCol);
-console.log(hourCol);
+var hourClmn = $("<div>");
+hourClmn.text(hours);
+hourClmn.attr("class", "hour col");
+mainRow.append(hourClmn);
+console.log(hourClmn);
 
-//inside each hour col, schedule input
 var scheduleInput = $("<input>");
 scheduleInput.attr("class", "event col-9");
-row.append(scheduleInput);
+mainRow.append(scheduleInput);
 console.log(scheduleInput);
 
-//Save btn div attaches to row
 var saveBtn = $("<div>");
 saveBtn.attr("class", "saveBtn col");
-row.append(saveBtn);
+mainRow.append(saveBtn);
 console.log(saveBtn);
 
-//Actual button attaches to save btn div
-var Btn = $("<button>")
-Btn.attr("class", "fas fa-save");
-saveBtn.append(Btn);
+var btn = $("<button>");
+btn.attr("class", "fas fa-save");
+saveBtn.append(btn);
+console.log(btn);
 
-// append to container
-$(schedule).append(row);
+$(schedule).append(mainRow);
+
 };
+
+
+
+
+
+
+
+
+
+
+
+// var timeOfDay = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM"];
+// for (var i = 0; i < timeOfDay.length; i++) {
+// var row = $("<div>"); 
+// row.attr("class","row");
+// console.log(timeOfDay);
+
+// //inside of each row
+// var hourCol = $("<div>");
+// hourCol.text(timeOfDay[i]);
+// hourCol.attr("class", "hour col");
+// row.append(hourCol);
+// console.log(hourCol);
+
+// //inside each hour col, schedule input
+// var scheduleInput = $("<input>");
+// scheduleInput.attr("class", "event col-9");
+// row.append(scheduleInput);
+// console.log(scheduleInput);
+
+// //Save btn div attaches to row
+// var saveBtn = $("<div>");
+// saveBtn.attr("class", "saveBtn col");
+// row.append(saveBtn);
+// console.log(saveBtn);
+
+// //Actual button attaches to save btn div
+// var Btn = $("<button>")
+// Btn.attr("class", "fas fa-save");
+// saveBtn.append(Btn);
+
+// // append to container
+// $(schedule).append(row);
+// };
+
+
