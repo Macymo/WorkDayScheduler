9895871eab2221eeb5af93418b9be9ab6b58be2b
Macy <59153195+Macymo@users.noreply.github.com>
2020-04-10 18:22:48 -0400

    cleaned up code

--- script.js
+++ script.js
@@ -1,45 +1,47 @@
 $(document).ready(function(){
-//current date/time for top of scheduler
-function getDate(){
-    $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
-};
 
-//set interval timer that updates page every second
-setInterval(getDate,1000);
+    var hour = moment().hours();
 
-var hour = moment().hours();
-//console.log(hour);
-
-$("input").each(function(){
-    var rowHour = $(this).attr("id")
-    var rowNumber = parseInt(rowHour);
-    //console.log(rowHour);
-    if (rowNumber === hour){
-        $(this).addClass("present");
-    } else if (rowNumber < hour){
-        $(this).addClass("past");
-    } else {
-        $(this).addClass("future");
+    //Moment.js date for top of page
+    function getDate(){
+        $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
     };
-});
 
-$(".saveBtn").click(function(){
-    var scheduleInputs = $(this).siblings(".event").val();
-    //console.log(scheduleInputs);
-    var inputsLocation = $(this).siblings(".event").attr("id");
-    //console.log(inputsLocation);
-    localStorage.setItem(inputsLocation,scheduleInputs);
-});
+    //Color coding schedule dependant upon hour
+    function colorSchedule(){
+        $("input").each(function(){
+            var rowHour = $(this).attr("id");
+            var rowNumber = parseInt(rowHour);
+            if (rowNumber === hour){
+                $(this).addClass("present");
+            } else if (rowNumber < hour){
+                $(this).addClass("past");
+            } else {
+                $(this).addClass("future");
+            };
+        });
+    };
 
-function renderStoredInputs(){
-    $(".event").each(function(){
-        var inputId = $(this).attr("id")
-        //console.log(inputId);
-        $(this).val(localStorage.getItem(inputId));
-        //console.log(localStorage.getItem(inputId));
+    //Rendering stored inputs after page refresh
+    function renderStoredInputs(){
+        $(".event").each(function(){
+            var inputId = $(this).attr("id");
+            $(this).val(localStorage.getItem(inputId));
+        });
+    };
+
+    //Click event to save user input in local storage
+    $(".saveBtn").click(function(){
+        var scheduleInputs = $(this).siblings(".event").val();
+        var inputsLocation = $(this).siblings(".event").attr("id");
+        localStorage.setItem(inputsLocation,scheduleInputs);
     });
-};
-renderStoredInputs();
+    
+    setInterval(getDate,1000);
+    colorSchedule();
+    setInterval(colorSchedule,1000);
+    renderStoredInputs();
+
 });
 
 
